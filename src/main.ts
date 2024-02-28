481a784cad2223c8180d02edbafd092c95f4fceb
Ing. Camilo Ortega <camilo.ortega@simerelectronics.com>
2024-02-28 08:25:48 -0500

    Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { AuthService } from './auth/auth.service';
+import { NestExpressApplication } from '@nestjs/platform-express'
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+
+  const authService = app.get(AuthService)
+  await authService.createInitialPermissions()
+  await authService.createInitialRoles()
+  await authService.createInitialUser()
+
+  await app.listen(4001);
+}
+bootstrap();
